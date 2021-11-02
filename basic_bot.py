f9a8e64432c07fa4e9ae29af14db2cf32247a108
yelinhui666 <73944528+yelinhui666@users.noreply.github.com>
2021-11-02 20:52:42 +0800

    Add files via upload

--- basic_bot.py
+++ basic_bot.py
@@ -10,15 +10,26 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    
-    #Ignore your own information, prevent endless loops
+    # Ignore your own information, prevent endless loops
     if message.author == client.user:
         return
-    
-    #Simple auto reply
+
+    # Simple auto reply
     if message.content == '嗨':
         await message.channel.send('你好呀')
     if message.content == '我是你爹':
         await message.channel.send('我是你儿子')
+    if message.content == '1':
+        await message.channel.send('2')
+    if message.content == 'Urgot':
+        await message.channel.send('You cannot know strength... Until you are broken')
 
-client.run('Your Token')#Write your Token here
+    if message.content.startswith("say"):
+        tmp = message.content.split(" ", 1)
+        if len(tmp) == 1:
+            await message.channel.send(f"{message.author.mention} 你要我說什麼啦？")
+        else:
+            await message.channel.send(f"{message.author.mention} 他逼得我說「{tmp[1]}」")
+
+
+client.run('OTA0OTg4NDkyMDM0MTAxMjU4.YYDiNg.OPiz1ZRd5bU3DNsHeTFNKo4KfT0')  # Write your Token here
